aa900ce58e6f249b88a624de34e5cdad41ecc8bb
RodrigoJose-Dev <contatorodrigojose123@gmail.com>
2021-10-11 20:54:45 -0300

    Iniciando Projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+    </div>
+  );
+}
+
+export default App;
